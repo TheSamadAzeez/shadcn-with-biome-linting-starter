e1fba4bb7b327223bc020125bb84e182f4c661b4
Samad Azeez <azeezsamad2004@gmail.com>
2025-11-29 07:48:21 +0100

    Update lint-staged configuration to filter files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,json,css}': ['biome check --write --no-errors-on-unmatched'],
+  '*.{js,jsx,ts,tsx,json,css}': (filenames) => {
+    const files = filenames.filter((f) => !f.includes('src/components/ui'))
+    if (files.length === 0) return []
+    return `biome check --write --no-errors-on-unmatched ${files.join(' ')}`
+  },
 }
